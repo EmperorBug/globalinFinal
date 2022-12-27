b9fa33f781b367f4473be9daae10a24ceae596e4
EmperorBug <38933900+EmperorBug@users.noreply.github.com>
2022-12-27 12:59:54 +0900

    수

--- src/main/java/com/global/kepla/controller/MainController.java
+++ src/main/java/com/global/kepla/controller/MainController.java
@@ -2,14 +2,16 @@ package com.global.kepla.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
-
 public class MainController {
 	
 	@GetMapping("/")
 	public String hello() {
+		log.info("main()......");
 		return "main";
 	}
 	
--- src/main/java/com/global/testclass.java
+++
@@ -1,7 +0,0 @@
-package com.global;
-
-public class testclass {
-	public static void main(String[] args) {
-		System.out.println("First Commit");
-	}
-}
