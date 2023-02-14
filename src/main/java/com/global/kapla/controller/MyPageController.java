0fe2cb9af5aaeb039300dbeb4bdd76d580383227
FoolLady <48585124+foolladys@users.noreply.github.com>
2023-02-14 14:28:49 +0900

    회원정보 변경 프론트만 일단 생성
    가져오는것 : userID
    
    해야할것 -> 비밀번호 복호화 후 입력한 값과 같은지 검사한 뒤에 다음 개인정보 바꾸는 페이지로 넘어감.

--- src/main/java/com/global/kapla/controller/MyPageController.java
+++ src/main/java/com/global/kapla/controller/MyPageController.java
@@ -1,16 +1,41 @@
 package com.global.kapla.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.global.kapla.service.UserService;
+import com.global.kapla.vo.UserVO;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 @RequestMapping("/mypage")
 public class MyPageController {
 	
+	@Autowired
+	private UserService userService;
+	
 	@GetMapping("/orderList")
 	public String orderList() {
 		return "mypage/order_list";
+	}
+	
+	@GetMapping("/modify_information")
+	public String modify_information(Model model, UserVO userVO, Principal principal) throws Exception {
+		
+		String id = principal.getName();
+		userVO.setId(id);
+		
+		model.addAttribute("user_info", userService.printUserName(userVO));
+		log.info(userVO.getId());
+		
+		return "mypage/modify_information";
 	}
 
 }
